9e1b72eea95a4364cb9d3af4f2b4f09c8fa31c59
maral <ashirova310197@gmail.com>
2020-10-31 14:22:33 +0200

    added Object Page for login page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,19 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Chrome, Firefox
+from typing import Union
+
+from pages.locators import MainPageLocators
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser: Union[Chrome, Firefox], url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
     def open(self):
         self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,11 @@
 from pages.main_page import MainPage
 
+LOGIN_URL = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()
 
@@ -13,6 +15,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-
